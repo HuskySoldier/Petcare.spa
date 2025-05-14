17307f02c2c644a5c2a33d1fac11ae9574962a63
Gabriel C. <ga.caroca@duocuc.cl>
2025-05-13 21:48:33 -0400

    Creacion de inventario
    Se creo las validaciones junton con lo necesario de que se esta pidiendo.
    Se tiene que corregir el dto y el feign que seria para el final, y corregir application.properties

---
+++ Inventario/src/main/java/com/example/Inventario/dto/ClientesDTO.java
@@ -0,0 +1,13 @@
+package com.example.Inventario.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class ClientesDTO {
+
+    private Long id;
+
+    private String nombre;
+}
--- Inventario/src/main/java/com/example/Inventario/service/InventarioService.java
+++ Inventario/src/main/java/com/example/Inventario/service/InventarioService.java
@@ -1,5 +1,52 @@
 package com.example.Inventario.service;
 
-public class InventarioService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.Inventario.model.Inventario;
+import com.example.Inventario.repository.InventarioRepository;
+
+@Service
+public class InventarioService {
+    @Autowired
+    private InventarioRepository inventarioRepository;
+
+    // Crear nuevo inventario
+    public Inventario crearInventario(Inventario inventario) {
+        return inventarioRepository.save(inventario);
+    }
+
+    // Obtener toda la cantidad de productos en un inventario
+    public List<Inventario> getInventario(int stock_actual) {
+        return (List<Inventario>) inventarioRepository.findByInventario(stock_actual);
+    }
+    
+
+    // Obtener un inventario por su ID
+    public Optional<Inventario> obtenerInventarioPorId(Long id_producto) {
+        return inventarioRepository.findById(id_producto);
+    }
+
+    // Actualizar el inventario
+    public Inventario actualizarInventario(Long id_producto, Inventario inventarioActualizada) {
+        if (inventarioRepository.existsById(id_producto)) {
+            inventarioActualizada.setId_inventario(id_producto);
+            return inventarioRepository.save(inventarioActualizada);
+        } else {
+            throw new RuntimeException("inventario no encontrado");
+        }
+    }
+
+    // Borrar un inventario
+    public Boolean deleteInventario(Long id_producto) { 
+    if (!inventarioRepository.existsById(id_producto)) {
+        throw new RuntimeException("Inventario no encontrado");
+    }
+    inventarioRepository.deleteById(id_producto);
+    return true;
+    }
+    
 }
