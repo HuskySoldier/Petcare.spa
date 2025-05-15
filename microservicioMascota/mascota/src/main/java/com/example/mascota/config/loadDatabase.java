5af1bd7ac25bc181b5506beedb115c6708578bfb
JosefaNL <jos.nunezl@duocuc.cl>
2025-05-14 21:19:41 -0400

    integracion mascotas
    integracion de especies, raza y modificaciones del codigo

--- microservicioMascota/mascota/src/main/java/com/example/mascota/config/loadDatabase.java
+++ microservicioMascota/mascota/src/main/java/com/example/mascota/config/loadDatabase.java
@@ -4,18 +4,23 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.mascota.model.Especie;
+import com.example.mascota.model.Mascota;
 import com.example.mascota.model.Raza;
+import com.example.mascota.repository.especieRepository;
+import com.example.mascota.repository.mascotaRepository;
 import com.example.mascota.repository.razaRepository;
 
 @Configuration
 public class loadDatabase {
 
     @Bean
-    CommandLineRunner initDatabase(razaRepository razaRepo){
+    CommandLineRunner initDatabase(razaRepository razaRepo, especieRepository especieRepo, mascotaRepository mascRepo){
         return args ->{
             //si no hay registros en la tablas
-            if(razaRepo.count() == 0){
-                //insertar los roles por defecto
+            if(razaRepo.count() == 0 && especieRepo.count() == 0){
+                //insertar los raza de perro por defecto
+                //raza de perro
                 Raza PasAle= new Raza();
                 PasAle.setNombreRaza("Pastor Alemán");
                 razaRepo.save(PasAle);
@@ -24,13 +29,55 @@ public class loadDatabase {
                 bull.setNombreRaza("Bulldog");
                 razaRepo.save(bull);
 
-                Raza PasAle= new Raza();
-                PasAle.setNombreRaza("Pastor Alemán");
-                razaRepo.save(PasAle);
+                Raza husSib= new Raza();
+                husSib.setNombreRaza("Husky siberiano");
+                razaRepo.save(husSib);
+
+            // raza de gatos
+                Raza persa= new Raza();
+                persa.setNombreRaza("Gato persa");
+                razaRepo.save(persa);
+
+                Raza rag= new Raza();
+                rag.setNombreRaza("Ragdoll");
+                razaRepo.save(rag);
+
+                Raza bom= new Raza();
+                bom.setNombreRaza("Bombay");
+                razaRepo.save(bom);
+
+            // raza cuyo
+                Raza ted= new Raza();
+                ted.setNombreRaza("Cobaya Teddy");
+                razaRepo.save(ted);
+
+                Raza amer= new Raza();
+                amer.setNombreRaza("Cuyo americano");
+                razaRepo.save(amer);
+
+                Raza rex= new Raza();
+                rex.setNombreRaza("Cobaya rex");
+                razaRepo.save(rex);
+
+            // especies mascota
+                Especie perro= new Especie();
+                perro.setNombreEspecie("Perro");
+                especieRepo.save(perro);
+
+                Especie gato= new Especie();
+                gato.setNombreEspecie("Gato");
+                especieRepo.save(gato);
+
+                Especie cuyo= new Especie();
+                cuyo.setNombreEspecie("Cobaya");
+                especieRepo.save(cuyo);
+            
+            
 
                 //cargar dos usuarios por defecto (opcional)
-                userRepo.save(new Usuario(null, "vicros","123456",admin));
-                userRepo.save(new Usuario(null, "camiuo","1234",usuario));
+                mascRepo.save(new Mascota(null, "Pichicha",2, "Hembra", persa,gato));
+                mascRepo.save(new Mascota(null, "Yeremy",3,"Macho",bull,perro));
+                mascRepo.save(new Mascota(null, "Toby", 1,"Macho",ted,cuyo ));
                 System.out.println("Datos iniciales Cargados");
             }
             else{
--- microservicioMascota/mascota/src/main/java/com/example/mascota/model/Mascota.java
+++ microservicioMascota/mascota/src/main/java/com/example/mascota/model/Mascota.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -34,12 +33,12 @@ public class Mascota {
     @Column(length = 100, nullable= false)
     private String sexo;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "idRaza")
     @JsonIgnoreProperties("mascota")
     private Raza raza;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "idEspecie")
     @JsonIgnoreProperties("mascota")
     private Especie especie;
