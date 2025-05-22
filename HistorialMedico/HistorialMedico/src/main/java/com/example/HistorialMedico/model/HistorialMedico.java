b3a5ede23bb6b25d9347df9f3affbdd08b3a8454
JosefaNL <jos.nunezl@duocuc.cl>
2025-05-22 16:51:49 -0400

    eliminacion de import
    elimine algunos import que no se estaban utilizando

--- HistorialMedico/HistorialMedico/src/main/java/com/example/HistorialMedico/model/HistorialMedico.java
+++ HistorialMedico/HistorialMedico/src/main/java/com/example/HistorialMedico/model/HistorialMedico.java
@@ -1,7 +1,6 @@
 package com.example.HistorialMedico.model;
 
 import java.sql.Date;
-import java.util.List;
 
 
 
--- HistorialMedico/HistorialMedico/src/main/java/com/example/HistorialMedico/model/Tratamiento.java
+++ HistorialMedico/HistorialMedico/src/main/java/com/example/HistorialMedico/model/Tratamiento.java
@@ -2,15 +2,13 @@ package com.example.HistorialMedico.model;
 
 import java.sql.Date;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
