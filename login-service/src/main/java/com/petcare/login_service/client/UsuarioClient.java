46247762df6304eab749e48bb1bbc19a51aecdfb
HuskySoldier <iva.hernandezl@duocuc.cl>
2025-06-13 19:33:50 -0400

    login
    loguea

--- login-service/src/main/java/com/petcare/login_service/client/UsuarioClient.java
+++ login-service/src/main/java/com/petcare/login_service/client/UsuarioClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "usuario-service", url = "http://localhost:8081")  // URL de UsuarioService
+@FeignClient(name = "usuario-service", url = "http://localhost:8082")  // URL de UsuarioService
 public interface UsuarioClient {
 
     
