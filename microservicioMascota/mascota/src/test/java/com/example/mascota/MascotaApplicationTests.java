a898fcdeda5b67baf10d0a33f25f30696d81f0cd
JosefaNL <jos.nunezl@duocuc.cl>
2025-06-23 20:56:48 -0400

    cambios mascota
    cambio en mascota

--- microservicioMascota/mascota/src/test/java/com/example/mascota/MascotaApplicationTests.java
+++ microservicioMascota/mascota/src/test/java/com/example/mascota/MascotaApplicationTests.java
@@ -1,13 +1,10 @@
 package com.example.mascota;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MascotaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+	
 }
